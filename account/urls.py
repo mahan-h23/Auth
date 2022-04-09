f6f830c98e8615fc0ae2bb5d7048a0543cad987f
Mahan Hassani <Mahan.Hassani@digikala.com>
2022-04-09 17:49:36 +0430

    Added Email Verify

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     re_path(r'^signup/$', views.signup, name='signup'),
-    path('login',)
+    path('login',views.login_view,name='login')
     # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
     #     views.activate, name='activate'),
 ]
